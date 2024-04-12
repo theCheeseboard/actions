35a0d812020b64349f472ac1020fa403743ab093
Victor Tran <vicr12345@gmail.com>
2024-04-12 20:33:47 +1000

    Build CMake project

--- build-cmake-project/builder.ts
+++ build-cmake-project/builder.ts
@@ -1,9 +1,39 @@
 import {BuilderOptions} from "./interfaces";
 import * as path from "node:path";
 import * as os from "node:os";
+import {exec} from "@actions/exec";
+import * as fs from "node:fs/promises";
 
-export function build(options: BuilderOptions) {
-    const projectFolder = path.join(os.homedir(), "git", "project-name");
+export async function build(options: BuilderOptions) {
+    const installFolder = path.join(os.homedir(), "install");
+    const installLibFolder = path.join(installFolder, "lib");
+    const projectFolder = path.join(os.homedir(), "git", options.project);
     const sourceFolder = path.join(projectFolder, "source", options.commitish);
+    const buildFolder = path.join(projectFolder, "build", options.arch, options.commitish);
 
+    await fs.mkdir(sourceFolder, {
+        recursive: true
+    });
+    await fs.mkdir(buildFolder, {
+        recursive: true
+    });
+
+    // Clone the source of the project
+    await exec("git", ["clone", `https://github.com/${options.project}.git`, sourceFolder])
+    await exec("git", ["checkout", options.commitish], {
+        cwd: sourceFolder
+    })
+
+    // Init CMake
+    const cmakeDefs: Record<string, string> = {
+        "CMAKE_INSTALL_PREFIX": installFolder,
+        "CMAKE_PREFIX_PATH": installLibFolder,
+        "CMAKE_INSTALL_RPATH": installLibFolder,
+    };
+
+    const cmakeArgs = [
+        ...Object.keys(cmakeDefs).map(def => `-D${def}=${cmakeDefs[def]}`)
+    ];
+    await exec("cmake", ["-B", buildFolder, "-S", sourceFolder, ...cmakeArgs]);
+    await exec("cmake", ["--install", buildFolder]);
 }
